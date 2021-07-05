4488a5d4c8c91ea89bf6c26440cc81480bdfde69
Mourad Kejji <mkejji@gmail.com>
2021-07-05 18:43:33 +0200

    Fixed indentation

--- index.mjs
+++ index.mjs
@@ -16,23 +16,23 @@ async function* search(dir, pattern) {
 }
 
 async function run() {
-	const browser = await puppeteer.launch({args: [
+  const browser = await puppeteer.launch({args: [
     '--disable-notifications',
     '--no-sandbox',
     '--disable-setuid-sandbox'
   ]});
   for await (const filename of search('kits', /\.html?$/i)) {
-		const id = uuid();
-		await fs.copyFile(filename, `out/${id}.html`);
-		const page = await browser.newPage();
-		await page.goto('file://' + filename).catch(e => console.log(`Error: ${filename} not loading\n${e.message}`));
-		await page.screenshot({
-			fullPage: true,
-			path: `out/${id}.png`
-		});
-		await page.close();
+    const id = uuid();
+    await fs.copyFile(filename, `out/${id}.html`);
+    const page = await browser.newPage();
+    await page.goto('file://' + filename).catch(e => console.log(`Error: ${filename} not loading\n${e.message}`));
+    await page.screenshot({
+      fullPage: true,
+      path: `out/${id}.png`
+    });
+    await page.close();
     console.log(`Saved ${id}`);
-	}
+  }
 }
 
 run();
